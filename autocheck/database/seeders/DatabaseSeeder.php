2c78033cec9453898dcbb07c47145e5bba78ecb3
Fodormt <fodormt@gmail.com>
2023-11-13 14:49:44 +0100

    seeder error

--- autocheck/database/seeders/DatabaseSeeder.php
+++ autocheck/database/seeders/DatabaseSeeder.php
@@ -14,11 +14,17 @@ class DatabaseSeeder extends Seeder
     {
         \App\Models\User::factory(10)->create();
 
-        \App\Models\User::factory()->create([
-            'name' => 'Admin',
-            'email' => 'test@example.com',
-            'is_admin' => true,
-            'is_premium' => true,
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Admin',
+        //     'email' => 'test@example.com',
+        //     'is_admin' => true,
+        //     'is_premium' => true,
+        // ]);
+
+        $this->call([
+            EventSeeder::class,
+            HistorySeeder::class,
+            VehichleSeeder::class,
         ]);
     }
 }
